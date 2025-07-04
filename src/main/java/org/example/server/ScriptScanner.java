461120cc7ccd7537f87a452c76cb33c45b19d0f8
Мартышов Данила Викторович <409091@niuitmo.ru>
2025-07-04 20:34:49 +0300

    Continued on MVP

--- src/main/java/org/example/server/ScriptScanner.java
+++ src/main/java/org/example/server/ScriptScanner.java
@@ -1,8 +1,11 @@
 package org.example.server;
 
+import java.nio.file.Path;
+
 public class ScriptScanner {
     public void scanScripts() {
         String classpath = System.getProperty("java.class.path");
         System.out.println("Scanning classpath: " + classpath);
+        Path workingDirectory = Paths.
     }
 }
